b5d84e391a913d1ff24436558fb7a1585e031da0
Gordon <gordon@docker.com>
2026-09-16 11:33:46 +0530

    chore: aggressively garbage collect 26+ leftover developer scratch scripts from repository root

--- remove_dup.mjs
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-let code = fs.readFileSync('d:/Web App - Aqua Blue/src/pages/WheelGame.tsx', 'utf8');
-
-// replace the first occurrence of the duplicate block
-code = code.replace("import { Canvas } from '@react-three/fiber';\nimport * as THREE from 'three';", "import { Canvas } from '@react-three/fiber';");
-
-fs.writeFileSync('d:/Web App - Aqua Blue/src/pages/WheelGame.tsx', code);
-console.log('Duplicate import removed!');

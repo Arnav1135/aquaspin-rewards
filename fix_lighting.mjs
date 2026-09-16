35d625faf91f53213618041b0b9010d07bb5c6bb
Gordon <gordon@docker.com>
2026-09-16 11:30:32 +0530

    chore: clean up scripts

--- fix_lighting.mjs
+++
@@ -1,6 +0,0 @@
-
-import fs from "fs";
-let light = fs.readFileSync("src/engine/3d/LightingSystem.tsx", "utf-8");
-light = light.replace("skyColor=", "color=");
-fs.writeFileSync("src/engine/3d/LightingSystem.tsx", light);
-
